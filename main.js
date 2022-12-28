260ca77dbabe0cf3b498f630efc09a8ad62e6ef4
daniel.cdlcz@gmail.com <daniel.cdlcz@gmail.com>
2022-12-27 23:39:54 -0500

    Segundo commit

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 function mobileMenu() {
     if (document.getElementById('menu').style.display == 'block') {
         document.getElementById('menu').style.display = 'none';
-
-
     }
     else {
         document.getElementById('menu').style.display = 'block';
         document.getElementById('menu').style.borderStyle = "solid";
+        document.getElementById('toggle').style.display = 'none' ;
+    }
+}
+function hideMobileMenu(){
+    if(document.getElementById('menu').style.display == 'block'){
+        document.getElementById('menu').style.display = 'none';
+        document.getElementById('toggle').style.display = 'block';
     }
 }
 document.getElementById('toggle').addEventListener('click', mobileMenu);
+document.getElementById('menu').addEventListener('click',hideMobileMenu);
